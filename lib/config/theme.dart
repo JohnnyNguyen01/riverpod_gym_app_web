6316249db7b3bb8fbbafcb70a00f7564026fa43c
johnny <johnnynguyenwork@gmail.com>
2021-04-07 11:15:58 +1000

    init commit

---
+++ lib/config/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Themes {
+  //primary theme
+  static final ThemeData primaryTheme = ThemeData(
+    primaryColor: Colors.red,
+    textTheme: GoogleFonts.poppinsTextTheme(),
+  );
+}
