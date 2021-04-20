b6c31665e8e969b094ecfefa92af1bf36197d922
johnny <johnnynguyenwork@gmail.com>
2021-04-20 23:08:51 +1000

    works but probs should refactor

--- lib/presentation/screens/library/create_program_screen/workout_builder/workout_builder_view.dart
+++ lib/presentation/screens/library/create_program_screen/workout_builder/workout_builder_view.dart
@@ -1,4 +1,7 @@
+import 'package:adonis_web_test/config/config.dart';
+import 'package:adonis_web_test/domain/models/models.dart';
 import 'package:adonis_web_test/states/workout_program/selected_week_session_state.dart';
+import 'package:adonis_web_test/states/workout_program/workout_program_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,9 +10,25 @@ class WorkoutBuilderView extends ConsumerWidget {
   Widget build(BuildContext context, ScopedReader watch) {
     final weekSessionState = watch(selectedWeekSessionStateProivder);
     return Expanded(
-        flex: 4,
-        child: weekSessionState == SelectedWeekSessionModel.init()
-            ? Container()
-            : Text(weekSessionState.toString()));
+      flex: 4,
+      child: weekSessionState == SelectedWeekSessionModel.init()
+          ? Container()
+          : WorkoutBuilder(),
+    );
+  }
+}
+
+class WorkoutBuilder extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final workoutProgramState = watch(workoutProgramStateProvider);
+    final weekSessionState = watch(selectedWeekSessionStateProivder);
+    return Column(
+      children: [
+        Text(
+            '${workoutProgramState.programName}: Week ${weekSessionState.week} Day ${weekSessionState.sessionNumber}',
+            style: kPageHeading)
+      ],
+    );
   }
 }
